54a2f2da0c684d9053a48af7ed3038ef81955999
Kotlovskiy <YA111EP@mail.ru>
2025-07-03 01:10:56 +0700

    feat
    Добавлены 2 экрана: список машин и детали о машине

--- app/src/main/java/com/unewexp/leasingapp/presentation/navigation/Container.kt
+++ app/src/main/java/com/unewexp/leasingapp/presentation/navigation/Container.kt
@@ -5,8 +5,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.unewexp.leasingapp.presentation.screens.CarScreen
 import com.unewexp.leasingapp.presentation.screens.CarsScreen
 import com.unewexp.leasingapp.presentation.screens.OrdersScreen
 import com.unewexp.leasingapp.presentation.screens.ProfileScreen
@@ -24,7 +27,19 @@ fun NavHostContainer(
         builder = {
 
             composable("cars") {
-                CarsScreen()
+                CarsScreen(
+                    onCardClick = { carId ->
+                        navController.navigate("car/${carId}")
+                    }
+                )
+            }
+
+            composable (
+                "car/{carId}",
+                arguments = listOf(navArgument("carId") { type = NavType.StringType })
+            ) { backStackEntry ->
+                val carId = backStackEntry.arguments?.getString("carId") ?: ""
+                CarScreen(carId, onToBack = {navController.popBackStack()}, onToBook = {})
             }
 
             composable("orders") {
